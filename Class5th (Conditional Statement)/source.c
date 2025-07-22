170c53350327e93f3e151ffca0a16172603d7695
pakerjaehyeong <paker1215@gmail.com>
2025-07-22 19:45:56 +0900

    2025.07.22진수를 저장하는 방볍

---
+++ Class5th (Conditional Statement)/source.c
@@ -0,0 +1,102 @@
+﻿#include <stdio.h>
+void main()
+{
+#pragma region 논리연산자 (수업 놓침 이슈 스위치 문 아래위치 할것)
+
+#pragma region AND 연산자
+
+	//int score = 90;
+	//char rank ='A';
+	//if (score == 90 && rank == 'A')
+	//{
+	//	printf("1st Place");
+	//}
+
+
+
+#pragma endregion
+
+#pragma region OR 연산자
+	// 두 개의 조건 중에서 하나라도 조건이 성립될 때 실행되는 연산자 입니다
+
+	//int weight = 70;
+	//char bodyfat = 30;
+	//
+	//if (weight > 75 || bodyfat > 20)
+	//{
+	//	printf("비만입니다");
+	//
+	//}
+
+
+
+#pragma endregion
+
+#pragma region NOT연산자
+	//하나의 조건을 반전시키는 연산자
+
+	//	int power = 0;
+	//	if ( !power == 1)
+	//	{
+	//		printf("참");
+	//	}	
+	//	else
+	//	{
+	//		printf("거짓");
+	//	}
+
+
+
+
+
+#pragma endregion
+
+
+
+
+
+
+#pragma endregion
+
+#pragma region 사분면 과제
+
+	//	int x = 1;
+	//	int y = 1;
+	//	
+	//	if (x > 0 && y > 0)
+	//	{
+	//		printf("제 1사분면");
+	//	}
+	//	else if (x < 0 && y > 0)
+	//	{
+	//		printf("제2사분면");
+	//	}
+	//	else if (x < 0 && y < 0)
+	//	{
+	//		printf("제3사분면");
+	//	}
+	//	else if (x > 0 && y < 0)
+	//	{
+	//		printf("제4사분면");
+	//	}
+	//	else if (x == 0 && y!=0)
+	//	{
+	//		printf("y 절편");
+	//	}
+	//	else if ( y == 0 && x!=0)
+	//	{
+	//		printf("x 절편");
+	//	}
+	//	else if (x == 0 && y == 0)
+	//	{
+	//		printf("원점");
+	//	}
+
+
+
+
+
+#pragma endregion
+
+
+}
