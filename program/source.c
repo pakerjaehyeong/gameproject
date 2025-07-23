26289cfaecaf77ef3e5fb312e2ea78daa0673872
pakerjaehyeong <paker1215@gmail.com>
2025-07-23 19:44:01 +0900

    2025.07.23반복문

---
+++ program/source.c
@@ -0,0 +1,143 @@
+#include<stdio.h>
+void main()
+{
+#pragma region 반복문
+
+	// 프로그램 내에서 특정한 작업을 반복적으로 
+	// 수행하는 명령문
+
+
+#pragma region 증감 연산자
+	// 피연산자를 하나씩 증가시키거나 감소시킬 떄
+	// 사용하는 연산자 입니다
+
+	// 전위 증감 연산자는 변수의 값을 증감시킨 후에
+	// 연산을 수행합니다 (++,--)
+	
+	
+	//  int previouseX = 0;
+	//  int x = 0;
+	//  
+	//  x = ++previouseX;
+	//  
+	//  printf("previousX 변수를 전위 증가한 값 :%d\n", previouseX);
+	//  printf("x 변수를 전위 증가한 값 : %d\n", x);
+	//  
+	//  x = --previouseX;
+	//  
+	//  printf("previousX 변수를 전위 감소한 값 : %d\n", previouseX);
+	//  printf("x 변수를 전위 감소한 값 : %d\n", x);
+	
+
+
+	// 후위 증감 연산자는 연산을 수행한 다음 변수의 값을 증감시킵니다
+
+	// int y = 0;
+	
+	// int previousY = 0;
+	
+	// y = previousY++;
+	
+
+	// printf("previousY 변수를 후위 증가한 값 : %d\n", previousY);
+	// printf("y 의 변수의 값:%d\n", y);
+	
+
+	// y = previousY--;
+	
+	// printf("previousY 변수를 후위 감소한 값 : %d\n", previousY);
+	// printf("y 의 변수의 값:%d\n", y);
+
+#pragma endregion
+
+
+#pragma region for문
+	// 초기식을 연산하여 조건식의 결과에 따라 특정한
+	// 횟수만큼 반복하는 반복문입니다
+
+	// for (int i = 0; i<5;i++ )
+	// {
+	// 	printf("update \n");
+	// 
+	// }
+#pragma endregion
+
+
+#pragma region while 문
+	// 특정 조건을 만족할 떄까지 계속해서 주어진 명령문을 
+	// 실행라는 반복문 입니다
+
+	// while문의 경우 위레서 아래로 실행되면 아래에 있는
+	// 명령문의 실행이 다 끝나면 다시 위에 있는 명령문으로
+	// 돌아가서 반복하는 구조입니다
+
+	// int count = 5;
+	// while (count > 0)
+	// {
+	// 	printf("%d\n", count--);
+	// 
+	// 
+	// }
+
+
+#pragma endregion
+
+
+#pragma region do-while문
+
+	// 조건과 상관없이 한 번의 작업을 수행한 다음 조건에
+	// 따라 명령문을 실행하는 반복문입니다
+
+	// int life = 0;
+	// 
+	// do
+	// {
+	// 
+	// 	printf("생존");
+	// 
+	// 
+	// } while (life > 0);
+
+	
+
+#pragma endregion
+
+
+#pragma region coutinue문
+	
+
+// for (int i = 0; i < 11; i++)
+// {
+// 	if (i % 3 == 0)
+// 	{ 
+// 		continue;
+// 	}
+// 	
+// 	printf("%d\n", i);
+// 
+// 
+// 
+// }
+
+
+
+
+
+#pragma endregion
+
+
+#pragma region 중첩 반복문
+// 반복문안에 반복문이 있는 구조 입니다
+
+
+
+#pragma endregion
+
+
+
+
+
+
+#pragma endregion
+
+}
