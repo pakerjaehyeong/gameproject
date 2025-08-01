af8b9e199306a45558b14709b668c1fea588de58
pakerjaehyeong <paker1215@gmail.com>
2025-08-01 19:46:46 +0900

    2025.08.01 재귀 함수

--- program/source.c
+++ program/source.c
@@ -23,7 +23,8 @@ void updata_position(int x,int y)
 	
 }
 
-void pirnt_status(char grade, int height, float temperature)
+
+void prirnt_status(char grade, int height, float temperature)
 {
 	printf("등급 : %c\n", grade);
 	printf("키 : %d\n", height);
@@ -32,6 +33,34 @@ void pirnt_status(char grade, int height, float temperature)
 
 
 }
+
+
+void swap(int * a, int * b)
+{
+	int c = *a;
+
+	
+	*a = *b;
+	*b = c;
+
+	
+}
+
+
+void find(int count)
+{
+	
+	
+	if(count <= 0)
+	{
+		return;
+	}
+	printf("%d\n", count);
+
+
+	find(count - 1);
+}
+
 
 void main()
 {
@@ -76,14 +105,54 @@ void main()
 	// 함수의 정의에서 전달받은 인수를 함수 내부로
 	// 전달하기 위해 사용하는 변수입니다
 
-	updata_position(36,-9);
+	// updata_position(36,-9);
 
 	// 메개 변수는 함수 내부에서만 연산이 이루어지며
-	// 함수가 
+	// 함수가 종료되면 메모리에서 해제됩니다
 
+	// prirnt_status('s', 175, 37.21f);
+
+	// 하나의 함수에 서로 다른 자료ㅗ
 
 
 #pragma endregion
+
+#pragma region 인수
+	// 힘수가 호출될 때 매개 변수에 실제로 전달되는 값입니다
+
+	
+
+	// 인수의 경우 함수에 있는 매개 변수에 따라 전달할 수
+	// 있는 인수의 수가 결정되며, 값을 전달하는 인수와 값을
+	// 전달 받는 매개 변수의 자료형이 서로 일치해야 합니다
+
+	// int x1 = 10;
+	// int y1 = 20;
+	// 
+	// printf("바뀌기 전 변수 x1의 값: %d%\n", x1);
+	// printf("바뀌기 전 변수 y1의 값: %d%\n", y1);
+	// 
+	// swap(&x1, &y1);
+	// 
+	// printf("변수 x1의 값: %d%\n", x1);
+	// printf("변수 y1의 값: %d%\n", y1);
+
+	
+#pragma endregion
+	
+#pragma region 재귀 함수
+	// 어떤 함수에서 자신을 다시 호출하여
+	// 작업을 수행하는 함수입니다
+
+	
+	// find(3);
+
+
+	// 재귀 함수는 함수를 계속 사용하기 때문에 스택 영역에
+	// 메모리가 계속 쌓이게 되므로 스택 오버플로우가 발생합니다
+#pragma endregion
+
+
 
 
 
